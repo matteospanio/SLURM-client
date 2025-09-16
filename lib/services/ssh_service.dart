0a172d57991c85f70ce98b9073da411d3d7c6562
Matteo <spartacus990@gmail.com>
2025-09-16 10:13:16 +0200

    fix multiple import as

--- lib/services/ssh_service.dart
+++ lib/services/ssh_service.dart
@@ -4,9 +4,9 @@ import '../models/connection.dart';
 import 'base_ssh_service.dart';
 
 // Conditional imports for web vs desktop
-import 'ssh_service_stub.dart' as stub
-    if (dart.library.io) 'ssh_service_io.dart' as io
-    if (dart.library.html) 'ssh_service_web.dart' as web;
+import 'ssh_service_stub.dart' as stub;
+import 'ssh_service_io.dart' as io;
+import 'ssh_service_web.dart' as web;
 
 // Export the base class and result types
 export 'base_ssh_service.dart';
@@ -20,5 +20,5 @@ BaseSSHService createSSHService() {
   }
 }
 
-// Type alias for backward compatibility  
+// Type alias for backward compatibility
 typedef SshService = BaseSSHService;
